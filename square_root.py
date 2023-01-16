172c0cbeb77e87191be9ea7f559460e77abaa0dc
jamescross813 <crossjames813@gmail.com>
2023-01-16 14:39:39 -0600

    func w/arg

--- square_root.py
+++ square_root.py
@@ -1,5 +1,6 @@
 # Write your square_root function here:
-
+def square_root(num):
+    
 # Uncomment these function calls to test your square_root function:
 #print(square_root(16))
 # should print 4
