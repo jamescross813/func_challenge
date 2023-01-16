ab8b2399a52785a7192c389bc2b7b36add627b65
jamescross813 <crossjames813@gmail.com>
2023-01-16 14:39:13 -0600

    setup

---
+++ square_root.py
@@ -0,0 +1,7 @@
+# Write your square_root function here:
+
+# Uncomment these function calls to test your square_root function:
+#print(square_root(16))
+# should print 4
+#print(square_root(100))
+# should print 10
