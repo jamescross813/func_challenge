1dae9d9f2bbda1e1b52a75d448e677bae62dc6ee
jamescross813 <crossjames813@gmail.com>
2023-01-17 22:10:18 -0600

    func w/arg

--- remainder.py
+++ remainder.py
@@ -1,5 +1,6 @@
 # Write your remainder function here:
-
+def remainder(num1, num2):
+    
 # Uncomment these function calls to test your remainder function:
 #print(remainder(15, 14))
 # should print 2
