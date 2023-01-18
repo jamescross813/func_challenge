01c0b7ff596a72360deb6c15f0da5d91411d66ea
jamescross813 <crossjames813@gmail.com>
2023-01-17 21:57:38 -0600

    setup

---
+++ win_percentage.py
@@ -0,0 +1,7 @@
+# Write your win_percentage function here:
+
+# Uncomment these function calls to test your win_percentage function:
+#print(win_percentage(5, 5))
+# should print 50
+#print(win_percentage(10, 0))
+# should print 100
