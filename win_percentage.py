98abd0429219716163eb9a3398918ee3b170f029
jamescross813 <crossjames813@gmail.com>
2023-01-17 22:05:02 -0600

    typo fix

--- win_percentage.py
+++ win_percentage.py
@@ -1,10 +1,10 @@
 # Write your win_percentage function here:
 def win_percentage(wins, losses):
     total_games = wins + losses
-    win_percent = (losses/total_games*100)
+    win_percent = (wins/total_games*100)
     return win_percent
 # Uncomment these function calls to test your win_percentage function:
-# print(win_percentage(5, 5))
+print(win_percentage(5, 5))
 # should print 50
-#print(win_percentage(10, 0))
+print(win_percentage(10, 0))
 # should print 100
