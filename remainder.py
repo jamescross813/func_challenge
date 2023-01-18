2275dec910db0f35b8fbb1963acc649148a9748e
jamescross813 <crossjames813@gmail.com>
2023-01-17 22:09:48 -0600

    setup

---
+++ remainder.py
@@ -0,0 +1,7 @@
+# Write your remainder function here:
+
+# Uncomment these function calls to test your remainder function:
+#print(remainder(15, 14))
+# should print 2
+#print(remainder(9, 6))
+# should print 0
