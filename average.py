07bb609f003b01d26a30c7e4d81261c36ce456f4
jamescross813 <crossjames813@gmail.com>
2023-01-17 22:07:14 -0600

    func w/arg

--- average.py
+++ average.py
@@ -1,5 +1,6 @@
 # Write your average function here:
-
+def average(num1, num2):
+    
 # Uncomment these function calls to test your average function:
 #print(average(1, 100))
 # The average of 1 and 100 is 50.5
