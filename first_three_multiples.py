a34d990cd25f703f0fdadd4d89c5fbb3cab21da7
jamescross813 <crossjames813@gmail.com>
2023-01-18 22:57:12 -0600

    setup

---
+++ first_three_multiples.py
@@ -0,0 +1,8 @@
+# Write your first_three_multiples function here
+
+
+# Uncomment these function calls to test your first_three_multiples function:
+#first_three_multiples(10)
+# should print 10, 20, 30, and return 30
+#first_three_multiples(0)
+# should print 0, 0, 0, and return 0
