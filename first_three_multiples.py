b361f0e8c563c33469fe9761aed2b532baa38ad0
jamescross813 <crossjames813@gmail.com>
2023-01-18 23:04:11 -0600

    fix infinite loop

--- first_three_multiples.py
+++ first_three_multiples.py
@@ -4,9 +4,10 @@ def first_three_multiples(num):
     while i<=3:
         curr = i*num
         print(curr)
+        i+=1
     return curr
 # Uncomment these function calls to test your first_three_multiples function:
-#first_three_multiples(10)
+# first_three_multiples(10)
 # should print 10, 20, 30, and return 30
 #first_three_multiples(0)
 # should print 0, 0, 0, and return 0
