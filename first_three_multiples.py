bd73ef77ff868fd17fb23eefbc14bf86f5861d08
jamescross813 <crossjames813@gmail.com>
2023-01-18 23:03:28 -0600

    atempt at working func

--- first_three_multiples.py
+++ first_three_multiples.py
@@ -1,7 +1,10 @@
 # Write your first_three_multiples function here
 def first_three_multiples(num):
-    
-
+    i=1
+    while i<=3:
+        curr = i*num
+        print(curr)
+    return curr
 # Uncomment these function calls to test your first_three_multiples function:
 #first_three_multiples(10)
 # should print 10, 20, 30, and return 30
