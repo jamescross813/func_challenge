29ee8b58c92d853ac4fa5271a95f6d5c48fb0927
jamescross813 <crossjames813@gmail.com>
2023-01-21 23:43:56 -0600

    func w/args

--- all_ops.py
+++ all_ops.py
@@ -1,5 +1,6 @@
 # Write your lots_of_math function here:
-
+def lots_of_math(num1, num2, num3, num4):
+    
 # Uncomment these function calls to test your lots_of_math function:
 #print(lots_of_math(1, 2, 3, 4))
 # should print 3, -1, -3, 0
