76eb4596bcd8ac3437e80c1b59ed14a41317fd8d
jamescross813 <crossjames813@gmail.com>
2023-01-21 23:36:53 -0600

    done

--- bond.py
+++ bond.py
@@ -3,7 +3,7 @@ def introduction(first, last):
     intro = last +", "+ first + " "+ last
     return intro
 # Uncomment these function calls to test your introduction function:
-#print(introduction("James", "Bond"))
+print(introduction("James", "Bond"))
 # should print Bond, James Bond
-#print(introduction("Maya", "Angelou"))
+print(introduction("Maya", "Angelou"))
 # should print Angelou, Maya Angelou
