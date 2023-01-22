92a3983e8196236ea954c93f81f6a4ae712b4230
jamescross813 <crossjames813@gmail.com>
2023-01-21 23:32:12 -0600

    setup

--- first_three_multiples.py
+++ first_three_multiples.py
@@ -7,7 +7,7 @@ def first_three_multiples(num):
         i+=1
     return curr
 # Uncomment these function calls to test your first_three_multiples function:
-# first_three_multiples(10)
+first_three_multiples(10)
 # should print 10, 20, 30, and return 30
-#first_three_multiples(0)
+first_three_multiples(0)
 # should print 0, 0, 0, and return 0
