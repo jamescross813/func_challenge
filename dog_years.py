57cd8125aa618395cbdda73bf40aea47d1a23271
jamescross813 <crossjames813@gmail.com>
2023-01-21 23:37:43 -0600

    setup

---
+++ dog_years.py
@@ -0,0 +1,7 @@
+# Write your dog_years function here:
+
+# Uncomment these function calls to test your dog_years function:
+#print(dog_years("Lola", 16))
+# should print "Lola, you are 112 years old in dog years"
+#print(dog_years("Baby", 0))
+# should print "Baby, you are 0 years old in dog years"
