df57b3e04d8ab535fde40e92ef13e6c7f35d27aa
jamescross813 <crossjames813@gmail.com>
2023-01-21 23:41:38 -0600

    working func

--- dog_years.py
+++ dog_years.py
@@ -1,8 +1,10 @@
 # Write your dog_years function here:
 def dog_years(name, age):
-    
+    age = age *7
+    statement = '{}, you are {} years old in dog years'.format(name, age)
+    return statement
 # Uncomment these function calls to test your dog_years function:
-#print(dog_years("Lola", 16))
+# print(dog_years("Lola", 16))
 # should print "Lola, you are 112 years old in dog years"
 #print(dog_years("Baby", 0))
 # should print "Baby, you are 0 years old in dog years"
