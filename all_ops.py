c2f5128d664e9c0bbff6d3426ef0705822eeb6e0
jamescross813 <crossjames813@gmail.com>
2023-01-21 23:48:04 -0600

    done

--- all_ops.py
+++ all_ops.py
@@ -1,8 +1,15 @@
 # Write your lots_of_math function here:
 def lots_of_math(a, b, c, d):
-
+    sum1=a+b
+    print (sum1)
+    sum2 = c-d
+    print (sum2)
+    sum3 = sum1*sum2
+    print (sum3)
+    sum4=sum3%a
+    return sum4
 # Uncomment these function calls to test your lots_of_math function:
-#print(lots_of_math(1, 2, 3, 4))
+print(lots_of_math(1, 2, 3, 4))
 # should print 3, -1, -3, 0
-#print(lots_of_math(1, 1, 1, 1))
+print(lots_of_math(1, 1, 1, 1))
 # should print 2, 0, 0, 0
