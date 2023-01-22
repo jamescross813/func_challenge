d4c4b5c741a718600ed9d89697548b657e268fea
jamescross813 <crossjames813@gmail.com>
2023-01-21 23:33:56 -0600

    working func

--- tip.py
+++ tip.py
@@ -1,8 +1,9 @@
 # Write your tip function here:
 def tip(total, percentage):
-
+    tip = (total/100)*percentage
+    return tip
 # Uncomment these function calls to test your tip function:
-#print(tip(10, 25))
+print(tip(10, 25))
 # should print 2.5
-#print(tip(0, 100))
+print(tip(0, 100))
 # should print 0.0
